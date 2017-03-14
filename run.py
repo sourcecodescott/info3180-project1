ef964e91757c08f59e85d39d10e57ec3855ba148
sourcecodescott <samath_scott@yahoo.com>
2017-03-14 00:22:41 -0400

    update run

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from api.profile_api import mod as prof_mod
 app.register_blueprint(prof_mod, url_prefix='')
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=8080)
